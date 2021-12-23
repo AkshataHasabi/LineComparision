f7ca21dcc8388b48538fbaf717a0f17dbaf0deca
AkshataHasabi <akshuh818@gmail.com>
2021-12-23 19:22:07 +0530

    ADDED:length of a line

--- LineComparisionUC.java
+++ LineComparisionUC.java
@@ -1,7 +1,13 @@
 public class LineComparisionUC {
 	public static void main(String[] args) {
-
-	System.out.println("Welcome to Line Comparision Computation");
+	int x1, x2, y1, y2;
+	double answer;
+	x1=3;
+	x2=6;
+	y1=9;
+	y2=4;
+	answer= Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	System.out.println(answer);
 	}
 }
 
