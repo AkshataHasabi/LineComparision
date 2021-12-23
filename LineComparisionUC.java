51a1ebb3e18d0965bee5a30f8cb7851873a5fa27
AkshataHasabi <akshuh818@gmail.com>
2021-12-23 19:59:59 +0530

    UPDATED:comparing two lengths

--- LineComparisionUC.java
+++ LineComparisionUC.java
@@ -1,8 +1,7 @@
 public class LineComparisionUC {
 	public static void main(String[] args) {
 	int x1, x2, x3, x4, y1, y2, y3, y4;
-	double answer;
-	double answer2;
+	
 	x1=2;
 	x2=4;
 	x3=3;
@@ -11,13 +10,18 @@ public class LineComparisionUC {
 	y2=6;
 	y3=5;
 	y4=7;
-	answer= Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-	answer2= Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
-	if(answer==answer2){
-		System.out.println("Lines are equall");
+	int Line1 = (int) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	int Line2= (int) Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+	int result = Integer.compare(Line1,Line2);
+	
+	if(result > 0){
+		System.out.println("Line1 is greater than Line2");
 	}
-	else {
-		System.out.println("Lines are not equall");
+	else if(result < 0){
+		System.out.println("Line1 is less than Line2");
+	}
+	else{
+		System.out.println("Line1 is equall to Line2");
 	}
 
 	}
