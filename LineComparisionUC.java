13248b1a6a69c3b8091db3c865974bf876e9a0e1
AkshataHasabi <akshuh818@gmail.com>
2021-12-23 19:39:25 +0530

    MODIFIED:checked lines equall or not

--- LineComparisionUC.java
+++ LineComparisionUC.java
@@ -1,13 +1,25 @@
 public class LineComparisionUC {
 	public static void main(String[] args) {
-	int x1, x2, y1, y2;
+	int x1, x2, x3, x4, y1, y2, y3, y4;
 	double answer;
-	x1=3;
-	x2=6;
-	y1=9;
-	y2=4;
+	double answer2;
+	x1=2;
+	x2=4;
+	x3=3;
+	x4=10;
+	y1=4;
+	y2=6;
+	y3=5;
+	y4=7;
 	answer= Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-	System.out.println(answer);
+	answer2= Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+	if(answer==answer2){
+		System.out.println("Lines are equall");
+	}
+	else {
+		System.out.println("Lines are not equall");
+	}
+
 	}
 }
 
